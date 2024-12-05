1de7fe35e480696b574e28f878b9ca25ab23042d
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-12-05 19:54:02 +0800

    feat: random generate circle object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import MobileJokeCard from "./components/MobileJokeCard";
 import useJoke from "./hooks/useJoke";
-import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import JokeCard from "./components/JokeCard";
 import Loading from "./components/Loading";
 import MoaiCircle from "./components/MoaiCircle";
@@ -10,45 +9,47 @@ const App = () => {
   const { joke, loading, fetchJoke } = useJoke();
   const [currentJoke, setCurrentJoke] = useState(null);
 
+  const [balls, setBalls] = useState([{ id: 0, x: 0, y: 0 }]);
+
+  const addBall = () => {
+    const newBall = {
+      id: balls.length,
+      x: Math.random() * window.innerWidth - 100,
+      y: Math.random() * window.innerHeight - 100,
+    };
+    setBalls((prevBalls) => [...prevBalls, newBall]);
+  };
+
   useEffect(() => {
     setCurrentJoke(joke?.joke);
   }, [joke]);
 
-  const controls = useAnimation();
-
-  useEffect(() => {
-    const bounce = () => {
-      controls.start({
-        x: [
-          Math.random() * window.innerWidth - 100,
-          Math.random() * window.innerWidth - 100,
-        ],
-        y: [
-          Math.random() * window.innerHeight - 100,
-          Math.random() * window.innerHeight - 100,
-        ],
-      });
-    };
-
-    bounce();
-    const interval = setInterval(bounce, 2000);
-
-    return () => clearInterval(interval);
-  }, [controls]);
-
   return (
     <section className="relative bg-gradient-to-br from-amber-50 to-orange-200 h-full">
-      <motion.div
-        animate={controls}
-        transition={{
-          duration: 2,
-          ease: "easeInOut",
-        }}
-        drag={true}
-        className="absolute w-20 h-20 rounded-full bg-amber-600"
-      ></motion.div>
+      <div>
+        {balls.map((ball) => (
+          <motion.div
+            key={ball.id}
+            initial={{ x: ball.x, y: ball.y }}
+            animate={{
+              x: Math.random() * window.innerWidth - 100,
+              y: Math.random() * window.innerHeight - 100,
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+            }}
+            drag={true}
+            whileHover={addBall}
+            onDragEnd={addBall}
+            onClick={addBall}
+            className="absolute w-20 h-20 rounded-full bg-amber-600"
+          />
+        ))}
+      </div>
+
       <div className="flex items-center justify-center h-full max-w-7xl mx-auto">
-        <div className=" mx-4 md:mx-0 lg:w-1/2">
+        <div className="mx-4 md:mx-0 lg:w-1/2">
           <div className="mx-10 lg:mx-0 block">
             <h1 className="text-4xl lg:text-7xl font-bold mb-4">
               You are a Dad? Get a joke for you now
