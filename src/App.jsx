157943a3b4c54184a7ea1c1a49eb6a3cc5fc06e2
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-12-06 12:50:54 +0800

    feat: added animation for main text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Loading from "./components/Loading";
 import MoaiCircle from "./components/MoaiCircle";
 import { useEffect, useState } from "react";
 
+const TEXT = ["You", "are", "a", "Dad?", "Get", "a", "joke", "for", "you"];
+
 const App = () => {
   const { joke, loading, fetchJoke } = useJoke();
   const [currentJoke, setCurrentJoke] = useState(null);
@@ -51,28 +53,46 @@ const App = () => {
       <div className="flex items-center justify-center h-full max-w-7xl mx-auto">
         <div className="mx-4 md:mx-0 lg:w-1/2">
           <div className="mx-10 lg:mx-0 block">
-            <h1 className="text-4xl lg:text-7xl font-bold mb-4">
-              You are a Dad? Get a joke for you now
-            </h1>
-            {loading ? (
-              <Loading />
-            ) : (
-              currentJoke && (
-                <AnimatePresence mode="wait">
+            <div>
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, staggerChildren: 0.2 }}
+                className="text-4xl lg:text-6xl font-bold mb-4 uppercase"
+              >
+                {TEXT.map((word, index) => (
+                  <motion.span
+                    key={index}
+                    initial={{ opacity: 0, x: -300 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.6, delay: 0.4 * index }}
+                  >
+                    {word}{" "}
+                  </motion.span>
+                ))}
+              </motion.h1>
+            </div>
+            <AnimatePresence>
+              {loading ? (
+                <Loading />
+              ) : (
+                currentJoke && (
                   <JokeCard
                     key={currentJoke}
                     joke={currentJoke}
-                    setCurrentJoke={setCurrentJoke}
                   />
-                </AnimatePresence>
-              )
-            )}
-            <button
+                )
+              )}
+            </AnimatePresence>
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2, delay: 3.5 }}
               className=" bg-amber-600 hover:bg-amber-700 text-white py-2 px-4 rounded font-medium my-4"
               onClick={fetchJoke}
             >
               Get a Dad Joke
-            </button>
+            </motion.button>
           </div>
         </div>
         <div className="hidden lg:block lg:w-1/2 md:relative">
--- src/components/JokeCard.jsx
+++ src/components/JokeCard.jsx
@@ -1,11 +1,10 @@
 import { motion } from "motion/react";
 import { useState } from "react";
 import { BsClipboard2CheckFill } from "react-icons/bs";
-import { IoMdClose } from "react-icons/io";
 import { LuClipboard } from "react-icons/lu";
 
 // eslint-disable-next-line react/prop-types
-const JokeCard = ({ joke, setCurrentJoke }) => {
+const JokeCard = ({ joke }) => {
   const [isCopied, setIsCopied] = useState(false);
   const handleCopy = () => {
     if (joke) {
@@ -21,17 +20,17 @@ const JokeCard = ({ joke, setCurrentJoke }) => {
     <motion.div
       initial={{ opacity: 0, scale: 0.2 }}
       animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.2, transition: { duration: 0.8 } }}
+      exit={{ opacity: 0, scale: 0.2, x: 100, transition: { duration: 0.8 } }}
       className="flex gap-6 mt-6 p-4 border border-amber-600 rounded-lg bg-amber-50"
     >
       <p className="text-2xl font-bold">{joke}</p>
       <div className="space-y-1">
-        <button
+        {/* <button
           className="h-fit bg-gray-500 hover:bg-gray-600 text-white p-2 rounded font-medium"
           onClick={() => setCurrentJoke(null)}
         >
           <IoMdClose />
-        </button>
+        </button> */}
         <motion.button
           whileTap={{ scale: 0.9 }}
           transition={{
